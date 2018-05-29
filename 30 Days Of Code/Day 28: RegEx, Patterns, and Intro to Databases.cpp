b15c012a42ae344494870ebbe7890f43efb5c4ec
Akash Macha <akashmacha1999@gmail.com>
2018-05-29 17:53:34 +0530

    Update Day 28: RegEx, Patterns, and Intro to Databases.cpp

--- 30 Days Of Code/Day 28: RegEx, Patterns, and Intro to Databases.cpp
+++ 30 Days Of Code/Day 28: RegEx, Patterns, and Intro to Databases.cpp
@@ -1,6 +1,6 @@
 /*
-Code has errors!
-Cannot complete this problem, i will give it a shot later.
+##  Code has errors!
+##  Cannot complete this problem, i will try it later.
 /*
 #include<bits/stdc++.h>
 
