f8249433876a662dc7b2f821968771e70a08db7a
Akash Macha <akashmacha1999@gmail.com>
2018-06-09 18:34:45 +0530

    Update Day 23: BST Level-Order Traversal.cpp
    Cleaned up, the code!

--- 30 Days Of Code/Day 23: BST Level-Order Traversal.cpp
+++ 30 Days Of Code/Day 23: BST Level-Order Traversal.cpp
@@ -1,5 +1,11 @@
 // https://www.hackerrank.com/challenges/30-binary-trees/problem
-
+/* 
+* When ever we hear the word BFS or Level_Order_Traversal, the DS which we use to implement it should flash in our mind.
+* ie. Queue
+* Then, just visualize the flow of the process (or) revise the procces
+* Remember, Repetition is the mother of learning!
+* Then try to implement it.
+*/
 #include <iostream>
 #include <cstddef>
 #include <queue>
@@ -37,22 +43,25 @@ class Solution{
         }
 // ---------Code-----------
 	void levelOrder(Node * root){
-
-        queue<Node*> q;
-        if(root == NULL)
-            return;
-        q.push(root);
-        while(!q.empty())
-        {
-            Node* current = q.front();
-            
-            cout << current->data << " ";
-            
-            if(current->left != NULL) q.push(current->left);
-            if(current->right != NULL) q.push(current->right);
-            
-            q.pop();
-        }
+		queue<Node*> q;
+		if(root == NULL)
+		    return;
+		q.push(root);
+		while(!q.empty())
+		{
+		    // Push the first Node ie. root node
+		    Node* current = q.front();
+			
+		    // Push the children corresponding to the current Node
+		    if(current->left != NULL) q.push(current->left);
+		    if(current->right != NULL) q.push(current->right);
+			
+		    // Visit the current Node
+		    cout << current->data << " ";
+			
+		    // Remove the current Node from the queue
+		    q.pop();
+		}
 	}
 //-----------------------------------
 };//End of Solution
